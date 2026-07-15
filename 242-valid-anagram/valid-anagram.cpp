d1820884262a53f52dddb3f7d3f1882994535f8c
Abhay Gupta <connectwithabhay32@gmail.com>
2026-07-15 10:03:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (92.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       sort(s.begin(),s.end());
-      sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
+        if (s.size() != t.size()) return false;
+        int counts[26] = {0};
+    
+        for (int i = 0; i < s.size(); i++) {
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (counts[i] != 0) return false;
+        }
+        
+        return true;
     }
 };
