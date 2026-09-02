662f1485333fd107edce29fd8ee29088a11811c1
Abhay Gupta <connectwithabhay32@gmail.com>
2026-09-02 09:58:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (74.32%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int maxalt = 0;
+        int newalt = 0;
+        
+        for (int i = 0; i < gain.size(); i++) {
+            newalt += gain[i];
+         maxalt = max(maxalt, newalt);
+        }
+        
+        return maxalt;
+    }
+};
