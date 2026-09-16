2b64035b35ec7ec1eeb7f7ffb7be226867cfb991
Abhay Gupta <connectwithabhay32@gmail.com>
2026-09-16 12:45:50 +0530

    Time: 24 ms (28.31%) | Memory: 81.5 MB (89.72%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0 ; i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                return true;
+                }
+                else{
+                    break;
+                } 
+                
+            }
+        }
+        return false;
+    }
+}
